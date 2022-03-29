4bbda25123976d80da3f9f9f4f3e8754828ccf57
devjohony <101238941+devjohony@users.noreply.github.com>
2022-03-29 21:33:59 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,3 +1,8 @@
+def create
+  binding.pry
+  Plan.create(plan_params)
+  redirect_to action: :index
+end
 class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
@@ -15,7 +20,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    
   end
 
   def getWeek
@@ -34,7 +39,13 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = # wdayメソッドを用いて取得した数値
+      if #「wday_numが7以上の場合」という条件式
+        wday_num = wday_num -7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdaysから値を取り出す記述}
       @week_days.push(days)
     end
 
